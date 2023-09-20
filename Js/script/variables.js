3b5469011b7d3e203e0ee0567d42ca80e4009ecb
NavpreetST <999navtoor@gmail.com>
2023-09-20 22:40:15 +0530

    js vars

---
+++ Js/script/variables.js
@@ -0,0 +1,11 @@
+let inbox1 = 100;
+let name = "Navpreet Singh";
+let inbox2 = 256;
+
+// alert(inbox)
+let allEmails = inbox1+ inbox2;
+// alert(allEmails);
+console.log(allEmails)
+
+const hella = "hello I am Admin here"
+console.log(hella);
